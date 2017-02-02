e99f8ab5650585fe33b6e01fd7f7d7ee7954730f
ozzyawaken <ozzy.knox@awakensoftware.com>
2017-02-02 16:52:05 +0100

    Imroved some code

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,13 @@
 var React = require('react');
 
-var Countdown = () => {
-    return (
-        <div>
-            <p>Countdown.jsx loaded</p>
-        </div>        
-    )
-};
+var Countdown = React.createClass({
+    render: () => {
+        return (
+            <div>
+                <p>Countdown.jsx loaded</p>
+            </div>        
+        )
+    }
+});
 
 module.exports = Countdown;
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,9 @@
 var React = require('react');
 
-var Timer = () => {
-    return (
-        <div>
-            <p>Timer.jsx loaded</p>
-        </div>
-    )
-};
+var Timer = React.createClass({
+    render: function () {
+        return (<p>Timer.jsx loaded</p>)
+    }
+});
 
 module.exports = Timer;
